545966e4e6361e66c59edfd1c516db29cb8ec41c
Uchechukwu <madumereuchechukwu@gmail.com>
2023-02-16 12:43:40 -0800

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
- * main = entry point
- * return: Always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0)
+	return (0);
 }
