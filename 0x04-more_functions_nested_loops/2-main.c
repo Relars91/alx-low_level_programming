860fe2a4805fe2fccacbb9e7dfc17c0264ac6fda
Uchechukwu <madumereuchechukwu@gmail.com>
2023-02-23 14:51:55 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for multiplying two integers.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%d\n", mul(89, 2410));
+	printf("%d\n", mul(-402, 4096));
+	return (0);
+}
