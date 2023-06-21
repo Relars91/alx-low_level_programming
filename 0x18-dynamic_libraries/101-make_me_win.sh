f37ef4ac1c8a6f296929f256e99e4132ebc5ebda
Uchechukwu Madumere <madumereuchechukwu@gmail.com>
2023-06-21 14:48:50 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Relars91/alx-low_level_programming/raw/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/tmp/nrandom.so
+ wget -q -O /tmp/librandy1.so https://github.com/Relars91/BatchScripts/raw/master/librandy1.so
+ export LD_PRELOAD=/tmp/librandy1.so
